bb9ee302a95bc2812e5b43d70ff18ada7d237100
ranjitkc7 <ranjit252@gmail.com>
2025-07-29 21:57:16 +0545

    create login page

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -1,53 +1,81 @@
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 import "../global.css";
+import React, { useState } from 'react'
 import { useRouter } from "expo-router";
+import { FontAwesome5, AntDesign} from '@expo/vector-icons';
 
 const RegisterPage = () => {
     const router = useRouter();
+    const [showPassword, setShowPassword] = useState(false);
+    const [showconfirmPassword, setShowConfirmPassword] = useState(false);
     return (
-        <View className="flex:1 justify-start items-center h-[100%]
-    bg-gray-300 p-[1rem]">
+        <View className="flex:1 justify-start items-center h-[100%] p-[6px]
+    bg-gray-300 ">
             <Text className="text-3xl font-bold 
             mt-[2rem] text-[#067eee]">Registration Form</Text>
             <Image
                 source={require('../assets/sign1.png')}
                 className="w-[27rem] h-[16rem] rounded-full"
             />
-            <TextInput
-                className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
+            <View className="w-full h-[22rem] rounded-md bg-blue-100 px-[5px]">
+                <TextInput
+                    className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
        focus:ring-[#023047] "
-                placeholder='Enter the username'
-                placeholderTextColor="#023047"
-            />
-            <TextInput
-                className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
+                    placeholder='Enter the username'
+                    placeholderTextColor="#023047"
+                />
+                <TextInput
+                    className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
        focus:ring-[#023047] "
-                placeholder='Enter the email'
-                placeholderTextColor="#023047"
-            />
-            <TextInput
-                className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
+                    placeholder='Enter the email'
+                    placeholderTextColor="#023047"
+                />
+                <View className="flex relative w-full">
+                    <TextInput
+                        className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
        focus:ring-[#023047] "
-                placeholder='Enter the password'
-                secureTextEntry
-                placeholderTextColor="#023047"
-            />
-            <TextInput
-                className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
+                        placeholder='Enter the password'
+                         secureTextEntry={!showPassword}
+                        placeholderTextColor="#023047"
+                    />
+                    <TouchableOpacity
+                        onPress={() => setShowPassword(!showPassword)}
+                        className="absolute top-[1.6rem] right-[10px]">
+                        {!showPassword ? (
+                            <AntDesign name="eye" size={30} color="black" />
+                        ) : (
+                            <FontAwesome5 name="eye-slash" size={22} color="black" />
+                        )}
+                    </TouchableOpacity>
+                </View>
+                <View className="flex relative w-full">
+                    <TextInput
+                        className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
        focus:ring-[#023047] "
-                placeholder='Re-enter the password'
-                secureTextEntry
-                placeholderTextColor="#023047"
-            />
-            <TouchableOpacity
-                className="bg-[#00FF00] px-4 py-2 mt-[1rem] rounded-[8px] w-full"
+                        placeholder='Re-type the password'
+                        secureTextEntry={!showconfirmPassword}
+                        placeholderTextColor="#023047"
+                    />
+                    <TouchableOpacity
+                        onPress={() => setShowPassword(!showconfirmPassword)}
+                        className="absolute top-[1.6rem] right-[10px]">
+                        {!showconfirmPassword ? (
+                            <AntDesign name="eye" size={30} color="black" />
+                        ) : (
+                            <FontAwesome5 name="eye-slash" size={22} color="black" />
+                        )}
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity
+                    className="bg-[#00FF00] px-4 py-2 mt-[1rem] rounded-[8px] w-full"
 
-            >
-                <Text className="text-[1.2rem] font-bold  text-center">
-                    Register
-                </Text>
-            </TouchableOpacity>
-            <View className=" flex-row mt-[1.5rem]">
+                >
+                    <Text className="text-[1.2rem] font-bold  text-center">
+                        Register
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <View className="flex-row  mt-[1rem]">
                 <Text className="text-[1.2rem]">If already have an account ? </Text>
                 <TouchableOpacity
                     className="pt-[-4px]"
--- my-app/app/logIn.jsx
+++ my-app/app/logIn.jsx
@@ -1,10 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import { useRouter } from "expo-router";
+import { FontAwesome5, AntDesign } from '@expo/vector-icons';
 
 const LoginPage = () => {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <View>
-      <Text>LoginPage</Text>
+    <View className="flex:1 justify-start items-center h-[100%]
+    bg-gray-300 p-[6px]">
+      <Text className="text-3xl font-bold 
+            mt-[2rem] text-[#067eee]">Login Form</Text>
+      <Image
+        source={require('../assets/sign1.png')}
+        className="w-[30rem] h-[18rem] rounded-full"
+      />
+      <View className="w-full h-[13rem] mt-[3rem] rounded-md bg-blue-100 px-[5px]">
+        <TextInput
+          className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
+       focus:ring-[#023047] "
+          placeholder='Enter the email'
+          placeholderTextColor="#023047"
+        />
+        <View className="flex relative w-full">
+          <TextInput
+            className="bg-white p-2 rounded-[6px] w-full mt-[1rem] h-[3.2rem] text-[1.1rem] focus:border-1
+       focus:ring-[#023047] "
+            placeholder='Enter the password'
+            secureTextEntry={!showPassword}
+            placeholderTextColor="#023047"
+          />
+          <TouchableOpacity
+            onPress={() => setShowPassword(!showPassword)}
+            className="absolute top-[1.6rem] right-[10px]">
+            {!showPassword ? (
+              <AntDesign name="eye" size={30} color="black" />
+            ) : (
+              <FontAwesome5 name="eye-slash" size={22} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          className="bg-[#00FF00] px-4 py-2 mt-[1rem] rounded-[8px] w-full"
+
+        >
+          <Text className="text-[1.2rem] font-bold  text-center">
+            Login
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className=" flex-row mt-[1.5rem]">
+        <Text className="text-[1.2rem]">If already haven't an account ? </Text>
+        <TouchableOpacity
+          className="pt-[-4px]"
+          onPress={() => router.back("/")}
+        >
+          <Text className="font-bold text-[1.1rem] text-[#067eee]">SignUp</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
